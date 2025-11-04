e9638a03e1a3391fe6f66a049ae33dda2928978c
Shritika Gauchan <gshritika24@tbc.edu.np>
2025-11-04 19:53:09 +0545

    static export issue

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Remove this if present:
+  // output: 'export',
+  // Or use:
+  // output: 'standalone',
+}
+
+export default nextConfig
